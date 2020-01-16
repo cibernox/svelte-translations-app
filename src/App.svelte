273421fb8a3aef199a8e96b264da499e3e81252c
Miguel Camba <miguel.camba@gmail.com>
2020-01-16 19:22:17 +0100

    Fix translation invocations

--- src/App.svelte
+++ src/App.svelte
@@ -5,13 +5,13 @@
 <main>
 	<h1>Translations:</h1>
 	<p>{$t("plain")}</p>
-	<p>{$t("interpolated")}</p>
-	<p>{$t("time")}</p>
-	<p>{$t("time-custom-format")}</p>
-	<p>{$t("date")}</p>
-	<p>{$t("date-custom-format")}</p>
-	<p>{$t("number")}</p>
-	<p>{$t("percent")}</p>
-	<p>{$t("pluralized")}</p>
-	<p>{$t("selected")}</p>
+	<p>{$t("interpolated", { values: { count: 3 } })}</p>
+	<p>{$t("time", { values: { now: new Date() } })}</p>
+	<p>{$t("time-custom-format", { values: { now: new Date() } })}</p>
+	<p>{$t("date", { values: { today: new Date() } })}</p>
+	<p>{$t("date-custom-format", { values: { today: new Date() } })}</p>
+	<p>{$t("number", { values: { n: new Date() } })}</p>
+	<p>{$t("percent", { values: { n: new Date() } })}</p>
+	<p>{$t("pluralized", { values: { count: 3 } })}</p>
+	<p>{$t("selected", { values: { gender: 'female' } })}</p>
 </main>
