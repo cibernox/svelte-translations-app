5e912bcc61cbf4b1c1add19ace900dfe84da4e54
Miguel Camba <miguel.camba@gmail.com>
2020-01-17 16:33:33 +0100

    Add inputs to modify arguments to translation files

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script>
 	import { t, locale, locales } from 'svelte-i18n';
+	const genders = ['male', 'female', 'other'];
+	let selectedGender = 'female';
+	let count1 = 3;
+	let count2 = 3;
 </script>
 
 <main>
 	<h1>Current locale is "{$locale}"</h1>
 	<p>{$t("plain")}</p>
-	<p>{$t("interpolated", { values: { count: 3 } })}</p>
+	<p>
+		{$t("interpolated", { values: { count: count1 } })}
+		<input type="number" bind:value={count1} min="0" max="20">
+	</p>
 	<p>{$t("interpolated", { values: { count: 0 } })} [zero]</p>
 	<p>{$t("interpolated", { values: { count: false } })} [false]</p>
 	<p>{$t("interpolated", { values: { count: null } })} [null]</p>
@@ -16,8 +23,16 @@
 	<p>{$t("date-custom-format", { values: { today: new Date() } })}</p>
 	<p>{$t("number", { values: { n: new Date() } })}</p>
 	<p>{$t("percent", { values: { n: new Date() } })}</p>
-	<p>{$t("pluralized", { values: { count: 3 } })}</p>
-	<p>{$t("selected", { values: { gender: 'female' } })}</p>
+	<p>
+		{$t("pluralized", { values: { count: count2 } })}
+		<input type="number" bind:value={count2} min="0" max="20">
+	</p>
+	<p>
+		{$t("selected", { values: { gender: selectedGender } })}
+		{#each genders as g}
+			<button type="button" disabled="{selectedGender === g}" on:click="{() => selectedGender = g}">{g}</button>
+		{/each}
+	</p>
 
 	{#each $locales as l}
 		<button type="button" disabled="{$locale === l}" on:click="{() => locale.set(l)}">{l}</button>
